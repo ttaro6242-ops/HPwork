d9d62b01109aec727441c0599f20ec32b182e74c
Volt0303 <volute@gmail.com>
2025-09-20 16:14:54 -0700

    fex:2:min

--- min.js
+++ min.js
@@ -5,6 +5,7 @@ const min = () =>{
                 <h1>Hello world!</h1>
                 <p>hello ttaro!! where are u ?</p>
                 <p>function added</p>
+                <p>function added again</p>
             </div>
         </>
     )
